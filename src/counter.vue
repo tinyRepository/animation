963fbbd3bd3ff1ea078131a11c6ee02068989383
tinybet <tiny@bk.ru>
2017-11-19 23:35:07 +0700

    added vars + autoprefixer

--- src/counter.vue
+++ src/counter.vue
@@ -16,6 +16,9 @@
 
 
 <style lang="scss">
+    $time: 6s;
+    $circle-size: 60px;
+    $circle-color: #58BAFC;
 
     // Markup elements
     .counter-wrap {
@@ -24,16 +27,26 @@
         border: 1px solid;
         padding: 100px 50px;
         position: relative;
+        display: -webkit-box;
+        display: -ms-flexbox;
         display: flex;
+        -webkit-box-orient: horizontal;
+        -webkit-box-direction: normal;
+        -ms-flex-direction: row;
         flex-direction: row;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
         align-items: center;
+        -webkit-box-pack: center;
+        -ms-flex-pack: center;
         justify-content: center;
     }
 
     .counter {
         position: relative;
+        display: -webkit-box;
+        display: -ms-flexbox;
         display: flex;
-        // overflow: hidden;
 
         &__number {
             position: absolute;
@@ -49,7 +62,7 @@
                 font-size: 16px;
                 color: #fff;
                 z-index: 10;
-                animation: number 6s;
+                animation: number $time;
             }
         }
 
@@ -57,8 +70,8 @@
             position: absolute;
             border-radius: 50%;
             z-index: 3;
-            width: 60px;
-            height: 60px;
+            width: $circle-size;
+            height: $circle-size;
             overflow: hidden;
             left: 0;
 
@@ -68,8 +81,8 @@
                 position: absolute;
                 border-radius: 50%;
                 z-index: 3;
-                width: 50px;
-                height: 50px;
+                width: $circle-size - 10;
+                height: $circle-size - 10;
                 left: 4px;
                 top: 4px;
                 background: transparent;
@@ -79,11 +92,11 @@
                 content: '';
                 display: block;
                 position: absolute;
-                background: #58BAFC;
-                width: 60px;
-                height: 60px;
+                background: $circle-color;
+                width: $circle-size;
+                height: $circle-size;
                 border-radius: 50%;
-                animation: fill 6s;
+                animation: fill $time;
                 left: 0;
                 top: 0;
             }
@@ -93,7 +106,7 @@
     .coin {
         &__svg {
             animation: coin 1s;
-            animation-iteration-count: 6;
+            animation-iteration-count: $time;
             animation-timing-function: cubic-bezier(0.6, 0.1, 0.59, 0.93);
             transform: translate3d(100%,0,0);
         }
@@ -146,87 +159,37 @@
         }
 
         @keyframes fill {
-            0% {
-                left: -100%;
-            }
+            0% {left: -100%;}
             // 1ый проход
-            14% {
-                left: -100%;
-            }
-            16% {
-                left: -70%;
-            }
-            18% {
-                left: -76%;
-            }
-            20% {
-                left: -72%;
-            }
+            14% {left: -100%;}
+            16% {left: -70%;}
+            18% {left: -76%;}
+            20% {left: -72%;}
             // 2ой проход
-            30% {
-                left: -72%;
-            }
-            32% {
-                left: -56%;
-            }
-            34% {
-                left: -62%;
-            }
-            36% {
-                left: -58%;
-            }
+            30% {left: -72%;}
+            32% {left: -56%;}
+            34% {left: -62%;}
+            36% {left: -58%;}
             // 3ий проход
-            46% {
-                left: -58%;
-            }
-            48% {
-                left: -42%;
-            }
-            50% {
-                left: -48%;
-            }
-            52% {
-                left: -44%;
-            }
+            46% {left: -58%;}
+            48% {left: -42%;}
+            50% {left: -48%;}
+            52% {left: -44%;}
             // 4ый проход
-            62% {
-                left: -44%;
-            }
-            64% {
-                left: -28%;
-            }
-            66% {
-                left: -34%;
-            }
-            68% {
-                left: -30%;
-            }
+            62% {left: -44%;}
+            64% {left: -28%;}
+            66% {left: -34%;}
+            68% {left: -30%;}
             // 5ый проход
-            78% {
-                left: -30%;
-            }
-            80% {
-                left: -14%;
-            }
-            82% {
-                left: -20%;
-            }
-            84% {
-                left: -16%;
-            }
+            78% {left: -30%;}
+            80% {left: -14%;}
+            82% {left: -20%;}
+            84% {left: -16%;}
             // 6ой проход
-            94% {
-                left: -16%;
-            }
-            96% {
-                left: 0%;
-            }
-            98% {
-                left: -6%;
-            }
-            100% {
-                left: -2%;
-            }
+            94% {left: -16%;}
+            96% {left: 0%;}
+            98% {left: -6%;}
+            100% {left: -2%;}
         }
 
         @keyframes coin {
